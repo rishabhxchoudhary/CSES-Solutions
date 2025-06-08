512f80ad49ad63e7cb65cf52a0eab85a03b9a316
Rishabh <rishabh26072003@gmail.com>
2025-06-08 15:45:32 +0530

    commit

--- Introductory Problems/Missing Number/answer.js
+++ Introductory Problems/Missing Number/answer.js
@@ -8,4 +8,10 @@ const nums = input[1].split(' ').map(Number);
 const expectedSum = n * (n + 1) / 2;
 const actualSum = nums.reduce((a, b) => a + b, 0);
 
+// OR
+// let actualSum = 0;
+// for (let num of nums) {
+//     actualSum += num;
+// }
+
 console.log(expectedSum - actualSum);
